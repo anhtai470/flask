08351532a458dae325dbfd84de90509b106b82f7
anhtai470 <22dh113195@st.huflit.edu.vn>
2025-07-15 14:28:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
-app = Flask(trananhtai)
+app = Flask(__name__)  # phải là __name__ (chuỗi có 2 dấu gạch dưới)
 
 @app.route('/')
 def home():
     return "10 diem nha thay!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
